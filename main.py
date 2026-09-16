54aa74716f68dee3f1886ae9fc413bc1ce185d6c
Amit Kulkarni <amit.kulkarni@sunbeaminfo.com>
2026-09-16 09:12:55 +0530

    test

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+
+def authenticate_user(db_conn, username, payload_str):
+    try:
+        # Dangerous code execution
+        config = eval(payload_str)
+        
+        # SQL Injection vulnerability
+        query = f"SELECT * FROM users WHERE username = '{username}'"
+        cursor = db_conn.cursor()
+        cursor.execute(query)
+        return cursor.fetchone()
+    except:
+        return None
+    
 def main():
     pass
 
